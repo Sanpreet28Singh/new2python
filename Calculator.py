f966bb13334aceb324d4da0a2198bd2bf35714f4
Sanpreet <30972869+Sanpreet28Singh@users.noreply.github.com>
2021-07-01 17:48:31 +0800

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,18 @@
+num1 = int(input("Enter first number :"))
+num2 = int(input("Enter second number :"))
+op= input("Enter operator  :")
+
+if op=='+':
+    print("Addition:=",(num1+num2))
+elif op=='-':
+    print("Subtraction:=",(num1-num2))
+elif op=='*':
+    print("Multiply:=",(num1*num2))
+elif op=='**':
+    print("Power:=",(num1**num2))
+elif op=='/':
+    print("Division:=",(num1/num2))
+elif op=='%':
+    print("Modulo:=",(num1%num2))
+elif op=='-':
+    print("Addition:=",(num1+num2))
